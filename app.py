ec6d8d62726948527a830ece7641ff0568b08cc7
Storozhuk Sergey <storozhuk@omnicomm.ru>
2017-04-18 18:08:01 +0300

    ошибка в режиме открытия файла

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class DownloadHandler(tornado.web.RequestHandler):
             fileSaveTmp.write(bookData)
             fileSaveTmp.close()
             subprocess.call(['java', '-jar', 'fb2epub-0.3.0.jar', '/tmp/' + name, '/tmp/' + name.replace('fb2', 'epub')])
-            bookData = open('/tmp/' + name.replace('fb2', 'epub'))
+            bookData = open('/tmp/' + name.replace('fb2', 'epub'), 'b')
             if savedFileOnMemory: savedFileOnMemory.pop()
             savedFileOnMemory.append(bookData)
             bookData.close()
